f61a11236aa7d3345c6a87c7cb6c9e38495ba37e
yemiancheng <ymc.github@gmail.com>
2019-08-11 21:35:55 +0800

    build(lint): lingting js code with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,60 @@
-var path = require('path');
-var webpack = require('webpack');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var ManifestPlugin = require('webpack-manifest-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
-var production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production';
 
 
-var externals = [];
+const externals = [];
 externals.push({
-    "lodash": {
-        commonjs: "lodash",
-        commonjs2: "lodash",
-        amd: "lodash",
-        root: "_"
-    }
+  lodash: {
+    commonjs: 'lodash',
+    commonjs2: 'lodash',
+    amd: 'lodash',
+    root: '_'
+  }
 });
 
-var plugins = [];
+const plugins = [];
 plugins.push(
-    //环境变量
-    new webpack.DefinePlugin({
-        'process.env': {
-            'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-        }
-    }),
-    //模块标识
-    production ? new webpack.HashedModuleIdsPlugin() : new webpack.NamedModulesPlugin(),
-    //文件清单
-    new ManifestPlugin({
-        name: 'manifest',
-        filename: 'manifest.json'
-    }),
-    //清理目录
-    new CleanWebpackPlugin('dist'),
-    //脚本压缩
-    new webpack.optimize.UglifyJsPlugin({
-        compress: production //minimize
-    }));
+  // 环境变量
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }
+  }),
+  // 模块标识
+  production ? new webpack.HashedModuleIdsPlugin() : new webpack.NamedModulesPlugin(),
+  // 文件清单
+  new ManifestPlugin({
+    name: 'manifest',
+    filename: 'manifest.json'
+  }),
+  // 清理目录
+  new CleanWebpackPlugin('dist'),
+  // 脚本压缩
+  new webpack.optimize.UglifyJsPlugin({
+    compress: production // minimize
+  })
+);
 
 
 module.exports = [{
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: production ? 'ymcCaptchaCode.umd.min.js' : 'ymcCaptchaCode.umd.js',
-        library: 'ymcCaptchaCode',
-        libraryTarget: 'umd'
-    },
-    externals: externals,
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }]
-    },
-    plugins: plugins
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: production ? 'ymcCaptchaCode.umd.min.js' : 'ymcCaptchaCode.umd.js',
+    library: 'ymcCaptchaCode',
+    libraryTarget: 'umd'
+  },
+  externals,
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  },
+  plugins
 }];
